20ce4b0e57a5a9c5b20198104fba06776f6a1d37
Stan Chen <stanchensz@gmail.com>
2024-06-13 00:32:38 -0600

    add single file

--- components/UploadDownload.tsx
+++ components/UploadDownload.tsx
@@ -33,6 +33,32 @@ const UploadDownload: React.FC<UploadDownloadProps> = ({ onFilesUpload, getFilte
         }
     };
 
+    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const fileList = event.target.files;
+        if (fileList) {
+            const filesArray = Array.from(fileList).map(file => ({
+                file,
+                relativePath: file.name
+            }));
+
+            const allFiles: { filename: any; code: string; }[] = [];
+
+            for (const fileObj of filesArray) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const content = e.target?.result as string;
+                    allFiles.push({ filename: fileObj.relativePath, code: content });
+
+                    // If all files are read, call the callback
+                    if (allFiles.length === filesArray.length) {
+                        onFilesUpload(allFiles);
+                    }
+                };
+                reader.readAsText(fileObj.file);
+            }
+        }
+    };
+
     const handleDownload = () => {
         const zip = new JSZip();
 
@@ -76,6 +102,29 @@ const UploadDownload: React.FC<UploadDownloadProps> = ({ onFilesUpload, getFilte
                     onMouseOut={e => e.currentTarget.style.backgroundColor = 'black'}
                 >
                     Upload Folder
+                </span>
+            </label>
+            <label style={{ display: 'block', marginBottom: '10px' }}>
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }}
+                />
+                <span
+                    style={{
+                        display: 'inline-block',
+                        width: '100%',
+                        backgroundColor: 'black',
+                        color: 'white',
+                        padding: '10px',
+                        textAlign: 'center',
+                        border: 'none',
+                        cursor: 'pointer'
+                    }}
+                    onMouseOver={e => e.currentTarget.style.backgroundColor = 'darkgray'}
+                    onMouseOut={e => e.currentTarget.style.backgroundColor = 'black'}
+                >
+                    Upload File
                 </span>
             </label>
             <button
